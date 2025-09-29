5de10eff9be954c570bcdad8a8b43d0a130aea50
Ω <om3ga6400.dev@gmail.com>
2025-09-29 23:05:32 +0000

    Make cards clickable

---
+++ js/cards.js
@@ -0,0 +1,10 @@
+var contentLis = document.querySelectorAll('.content li');
+contentLis.forEach(function(li) {
+    var link = li.querySelector('a');
+    if (link) {
+        li.style.cursor = 'pointer';
+        li.onclick = function() {
+            window.location.href = link.href;
+        };
+    }
+});
