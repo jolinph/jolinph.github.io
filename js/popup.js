516bf6fcf4f4292461d12e10eb51b6e54882718b
Ω <om3ga6400.dev@gmail.com>
2025-09-29 17:58:57 +0000

    lots of stuff

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,9 @@
 window.onload = function () {
-   if (!localStorage.getItem('popupClosed')) {
-      var p = document.getElementById('popup');
-      if (p) p.style.display = 'flex';
-   }
+   var p = document.getElementById('popup');
+   if (p) p.style.display = 'flex';
 };
 
 function closePopup() {
    var p = document.getElementById('popup');
    if (p) p.style.display = 'none';
-   try {
-      localStorage.setItem('popupClosed', 'true');
-   } catch (e) {}
 }
