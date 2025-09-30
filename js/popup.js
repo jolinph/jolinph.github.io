7f6c0b53d4e22282732f0b6dd75c9575b6d623a4
Ω <om3ga6400.dev@gmail.com>
2025-09-30 10:14:23 -0700

    Remove pop-up script

--- js/popup.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-   var p = document.getElementById('popup');
-   if (p) p.style.display = 'flex';
-};
-
-function closePopup() {
-   var p = document.getElementById('popup');
-   if (p) p.style.display = 'none';
-}
